56529d88142269bd919fef0caecfd4894661e5d3
Jingyao Zong <zongjingyao@unity3d.com>
2017-08-11 18:17:20 +0800

    [UnityAnaltyics] AnalyticsCheck.cs: print error message if Analytics is off after finishing build job in Editor(>=5.5)

--- UnityAnalytics/AnalyticsCheck.cs
+++ UnityAnalytics/AnalyticsCheck.cs
@@ -3,3 +3,24 @@
 #if !UNITY_ANALYTICS
 #error "Analytics is not enabled!"
 #endif
+
+//Versions <5.5 don't have UnityEditor.Analytics
+#if UNITY_EDITOR && UNITY_5_5_OR_NEWER
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Analytics;
+using UnityEditor.Callbacks;
+
+public class AnalyticsCheck
+{
+    [PostProcessBuildAttribute(0)]
+    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+    {
+        //Check Analytics state when build is done.
+        if (!AnalyticsSettings.enabled)
+        {
+            Debug.LogError("Analytics is not enabled!");
+        }
+    }
+}
+#endif
